8dc1c303f42923568ce29421f14753b16e4c40cf
Earl_Bao <429152352@qq.com>
2020-09-13 09:39:07 +0800

    testTools

---
+++ src/Tools/Times.java
@@ -0,0 +1,34 @@
+package Tools;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+* @Description: 测试方法耗时（效率）
+* @Param: method
+* @returnType: time
+* @Author: earl_Bao
+* @Date: 2020/9/12
+* @Version: 1.0
+*/
+public class Times {
+	private static final SimpleDateFormat fmt = new SimpleDateFormat("HH:mm:ss.SSS");
+	
+	public interface Task {
+		void execute();
+	}
+	
+	public static void test(String title, Task task) {
+		if (task == null) return;
+		title = (title == null) ? "" : ("【" + title + "】");
+		System.out.println(title);
+		System.out.println("开始：" + fmt.format(new Date()));
+		long begin = System.currentTimeMillis();
+		task.execute();
+		long end = System.currentTimeMillis();
+		System.out.println("结束：" + fmt.format(new Date()));
+		double delta = (end - begin) / 1000.0;
+		System.out.println("耗时：" + delta + "秒");
+		System.out.println("-------------------------------------");
+	}
+}
